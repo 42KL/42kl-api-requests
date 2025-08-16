e50aeb186204140f87df0e8db9ea02c180b84d80
yt <yealingt@42iskandarputeri.edu.my>
2025-08-16 17:38:31 +0800

    (feat): added compute_cursus_end_from_begin() function to compute cursus end date from cursus begin date based on predefined cursus duration

--- ftapi/FtCursus.py
+++ ftapi/FtCursus.py
@@ -3,13 +3,21 @@
 
 from datetime import datetime as dt, timedelta
 from FtApi import FtApi
+from FtDateTime import is_valid_date
 from FtUtils import ft_write_info
 
 
-IDS = {"c-piscine": 9,
-       "disco-piscine-ai": 79,
-       "disco-piscine-python": 80}
-DURATION = {"9": 26, "80": 5, "79": 3}
+IDS = {"42": 1,
+       "42cursus": 21,
+       "c-piscine": 9,
+       "c-piscine-brussels": 64,
+       "c-piscine-reloaded": 66,
+       "discovery-piscine-ai-fundamentals-for-all": 79,
+       "discovery-piscine-core-python-programming": 80,
+       "discovery-piscine-python": 69,
+       "discovery-piscine-web-programming-essentials": 3,
+       "piscine-c": 4}
+DURATION = {"9": 26, "79": 3, "80": 5, "3": 5}
 
 
 def get_cursus_users(ft_api: FtApi = None,
@@ -17,7 +25,8 @@ def get_cursus_users(ft_api: FtApi = None,
                      begin_date: str = None) -> dict:
     """return a dictionary of user data for user subscribed to the given
     cursus_id with a begin date within the range of the planned duration."""
-    assert ft_api is not None, "Invalid FtApi object."
+    if ft_api is None:
+        ft_api = FtApi()
     assert isinstance(ft_api, FtApi), "Invalid FtApi object."
     assert cursus_id is not None, "cursus_id not specified."
     assert isinstance(cursus_id, str), "cursus_id must be a string."
@@ -43,10 +52,31 @@ def get_cursus_users(ft_api: FtApi = None,
 def get_cursus_projects(ft_api: FtApi = None,
                         cursus_id: str = None) -> dict:
     """return a dictionary of project data for projects in given cursus."""
-    assert ft_api is not None and isinstance(ft_api, FtApi), \
-        "Invalid FtApi object."
+    if ft_api is None:
+        ft_api = FtApi()
+    assert isinstance(ft_api, FtApi), "Invalid FtApi object."
     assert cursus_id is not None, "cursus_id not specified."
     assert isinstance(cursus_id, str), "cursus_id must be a string."
     get_url = f"{ft_api.site}/v2/cursus/{cursus_id}/projects"
     ft_write_info(f"GET from {get_url}")
     return ft_api.get(get_url)
+
+
+def compute_cursus_end_from_begin(cursus_id: str = None,
+                                  begin_date: str = None) -> dt:
+    """Return cursus end_at from given begin_at based on DURATION constant"""
+    if cursus_id is None or not isinstance(cursus_id, str) or \
+            cursus_id not in DURATION or \
+            begin_date is None or not isinstance(begin_date, str) or \
+            not is_valid_date(begin_date):
+        return None
+    try:
+        begin_date = dt.strptime(begin_date, "%Y-%m-%d")
+        end_date = begin_date + timedelta(days=DURATION[cursus_id])
+        end_date -= timedelta(seconds=1)
+        end_date -= timedelta(hours=8)
+    except BaseException as error:
+        print(error)
+        return None
+    return str(end_date)
+
