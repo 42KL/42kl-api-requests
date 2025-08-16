8a00c4a2b8f1edd7c2059c9bf6d80ceb81b8a909
yt <yealingt@42iskandarputeri.edu.my>
2025-08-16 17:43:02 +0800

    (chore): update create_users.py according to FtUser dataclass version

---
+++ ftapi/FtCSV.py
@@ -0,0 +1,33 @@
+# FtCSV.py
+"""A collection of functions used with CSV file input"""
+
+import csv
+
+
+def read_csv_into_list(filename: str = None):
+    """Read given CSV filename and return list of rows."""
+    assert filename is not None, "CSV Filename not specified"
+    csv_data = list()
+    try:
+        with open(filename) as CSV_FILE:
+            read_buffer = csv.reader(CSV_FILE)
+            for row in read_buffer:
+                csv_data.append(row)
+            CSV_FILE.close()
+    except BaseException as error:
+        raise error
+    assert len(csv_data) > 0, f"No data read from {filename}"
+    return csv_data
+
+
+def get_index(target: str = None, source: list = None):
+    """Find and return the index of a string in a list of strings."""
+    assert target is not None, "Target string not defined"
+    assert source is not None, "Source strings not defined"
+    assert isinstance(target, str), "Target is not a string"
+    assert isinstance(source, list) and not isinstance(source, str), \
+        "Source is not a list of strings"
+    i = 0
+    while source[i] != target:
+        i += 1
+    return i
--- users/create_users.py
+++ users/create_users.py
@@ -8,7 +8,9 @@ import sys
 from time import sleep
 from FtApi import FtApi
 from FtCSV import read_csv_into_list
-from FtUser import FtUser
+from FtCursus import compute_cursus_end_from_begin
+from FtDateTime import is_valid_date
+from FtUser import FtUser, FtCursusUser
 from FtUtils import ft_write_error, ft_write_info, ft_write_success
 
 
@@ -21,58 +23,57 @@ def create_user(ft_user: FtUser = None, ft_api: FtApi = None):
         ft_api = FtApi()
     ft_write_info(f"POST to {ft_api.site}/v2/users")
     try:
-        response = ft_api.oauth.post(f"{ft_api.site}/v2/users",
-                                     json=ft_user.user)
+        user_dict = {"user": ft_user.asdict()}
+        response = ft_api.oauth.post(f"{ft_api.site}/v2/users", json=user_dict)
         if int(response.status_code) != 201:
             raise Exception("Failed: User creation failure")
     except BaseException as error:
         raise error
         return error
     response = json.loads(response.text)
-    message = f"User created for {ft_user.user['user']['email']}:"
+    message = f"User created for {ft_user.email}:"
     message += response["login"]
     ft_write_success(message)
     return response
 
 
-def add_user_to_cursus(ft_user: FtUser = None, ft_api: FtApi = None):
-    """Adds a user to cursus_users."""
-    assert ft_user is not None and isinstance(ft_user, FtUser), \
-        "Invalid FtUser object."
+def add_user_id_to_cursus(ft_api: FtApi = None,
+                          user_id: str = None,
+                          cursus_id: str = None,
+                          begin_date: str = None) -> dict:
+    """Create a cursus user using 42 API (v2)"""
+    assert user_id is not None and isinstance(user_id, str) and \
+        len(user_id) > 0, "Undefined/invalid user_id, doing nothing."
+    assert cursus_id is not None and isinstance(cursus_id, str) and \
+        len(cursus_id) > 0, "Undefined/invalid cursus_id, doing nothing."
+    assert begin_date is not None and isinstance(begin_date, str) and \
+        len(begin_date) > 0 and is_valid_date(begin_date), \
+        "Undefined/invalid begin_date, doing nothing."
     if ft_api is None or not isinstance(ft_api, FtApi):
         ft_api = FtApi()
-    ft_write_info(f"POST to {ft_api.site}/v2/cursus_users")
-    if "user_id" not in ft_user.cursus["cursus_user"] or \
-            ft_user.cursus["cursus_user"]["user_id"] is None:
-        ft_user.cursus["cursus_user"]["user_id"] = ft_user.user["user"]["id"]
-    try:
-        response = ft_api.oauth.post(f"{ft_api.site}/v2/cursus_users",
-                                     json=ft_user.cursus)
-        if int(response.status_code) != 201:
-            raise Exception("Failed: Cursus enrollment failure")
-    except BaseException as error:
-        raise error
-        return error
-    message = ft_user.user["user"]["login"] + " enrolled to "
-    message += ft_user.cursus["cursus_user"]["cursus_id"]
-    ft_write_success(message)
-    return json.loads(response.text)
+    BEGIN_AT = f"{begin_date} 00:00:42"
+    END_AT = compute_cursus_end_from_begin(cursus_id=cursus_id,
+                                           begin_date=begin_date)
+    cursus_user = FtCursusUser(user_id=user_id, cursus_id=cursus_id,
+                               begin_at=BEGIN_AT, end_at=END_AT)
+    cursus_user = {"cursus_user": cursus_user.asdict()}
+    post_url = f"{ft_api.site}/v2/cursus_users"
+    post_re = ft_api.oauth.post(post_url, json=cursus_user)
+    return post_re
 
 
 def main():
     """Creates external users on the intranet for Discovery Piscine
     """
-    if len(sys.argv) == 2:
-        CSV_FILE = sys.argv[1]
-    else:
-        CSV_FILE = "test.csv"
-    POOL_YEAR = "2025"
-    POOL_MONTH = "june"
-    BEGIN_AT = "2025-06-23T00:00:42.000Z"
-    END_AT = "2025-06-25T15:59:42.000Z"
-    CURSUS_ID = "79"  # This is Discovery AI
-
+    USAGE = f"Usage: python3 {sys.argv[0]} users.csv CURSUS_ID BEGIN_DATE"
+    SUPPORTED_CURSUS = ["79", "80"]
     try:
+        assert len(sys.argv) == 4, USAGE
+        CSV_FILE = sys.argv[1]
+        CURSUS_ID = sys.argv[2]
+        assert CURSUS_ID in SUPPORTED_CURSUS, f"\"{CURSUS_ID}\" not supported.\n{USAGE}"
+        BEGIN_DATE = sys.argv[3]
+        assert is_valid_date(BEGIN_DATE), f"\"{BEGIN_DATE}\" not a valid date.\n{USAGE}"
         csv_data = read_csv_into_list(CSV_FILE)
         for row in csv_data:
             first_name = row[0]
@@ -89,24 +90,30 @@ def main():
                              first_name=first_name,
                              last_name=last_name,
                              usual_first_name=usual_first_name,
-                             campus_id=ft_api.campus,
-                             pool_year=POOL_YEAR,
-                             pool_month=POOL_MONTH,
-                             cursus_id=CURSUS_ID,
-                             begin_at=BEGIN_AT,
-                             end_at=END_AT)
+                             campus_id=ft_api.campus)
             api_status = create_user(ft_user, ft_api)
-            if isinstance(api_status, str):
-                res.append(api_status)
+            if isinstance(api_status, Exception):
+                res.append(str(api_status))
+                print(",".join(res))
+                ft_api = None
+                del ft_api
                 continue
             res.append(api_status["login"])
-            ft_user.user["user"]["login"] = api_status["login"]
-            ft_user.user["user"]["id"] = api_status["id"]
             sleep(0.6)
-            api_status = add_user_to_cursus(ft_user, ft_api)
-            if isinstance(api_status, str):
-                res.append(api_status)
+            post_re = add_user_id_to_cursus(ft_api=ft_api,
+                                            user_id=str(api_status["id"]),
+                                            cursus_id=CURSUS_ID,
+                                            begin_date=BEGIN_DATE)
+            if int(post_re.status_code) != 201:
+                error = f"{post_re.status_code}: {post_re.reason}."
+                res.append(error)
+                print(",".join(res))
+                ft_api = None
+                del ft_api
+                raise Exception(error)
             print(",".join(res))
+            ft_api = None
+            del ft_api
             sleep(0.6)
     except BaseException as error:
         ft_write_error(f"Error: {error}")
