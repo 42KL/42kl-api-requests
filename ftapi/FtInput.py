0a01e8889b0f2388bc1cc939c89ddaf043d8f8dd
yt <yealingt@42iskandarputeri.edu.my>
2025-08-20 11:59:14 +0800

    (chore) Print prompts to stderr

--- ftapi/FtInput.py
+++ ftapi/FtInput.py
@@ -2,6 +2,7 @@
 """Collection of functions to request input"""
 
 from datetime import datetime as dt
+from sys import stderr
 from FtDateTime import is_valid_date
 from FtCursus import IDS as CURSUS_IDS
 from FtUtils import ft_write_error
@@ -10,17 +11,18 @@ from FtUtils import ft_write_error
 def read_input_cursus():
     """Request user input for a Cursus ID"""
     cursus_id = ""
-    print("================================================")
-    print("ID".rjust(4), end="")
-    print(" || Cursus")
-    print("================================================")
+    print("================================================", file=stderr)
+    print("ID".rjust(4), end="", file=stderr)
+    print(" || Cursus", file=stderr)
+    print("================================================", file=stderr)
     valid_ids = list()
     for cursus in CURSUS_IDS:
-        print(f"{str(CURSUS_IDS[cursus]):>4} || {cursus}")
+        print(f"{str(CURSUS_IDS[cursus]):>4} || {cursus}", file=stderr)
         valid_ids.append(str(CURSUS_IDS[cursus]))
-    print("================================================")
+    print("================================================", file=stderr)
     while cursus_id not in valid_ids:
-        cursus_id = input("Enter the Cursus ID: ")
+        print("Enter the Cursus ID: ", end="", file=stderr)
+        cursus_id = input()
         if cursus_id not in valid_ids:
             ft_write_error(f"{cursus_id} - Unsupported cursus ID")
     return cursus_id
@@ -30,7 +32,8 @@ def read_input_date() -> dt:
     """Request user input for a date string in YYYY-mm-dd format"""
     date_str = ""
     while not is_valid_date(date_str):
-        date_str = input(f"Enter a date in the format YYYY-mm-dd: ")
+        print("Enter a date in the format YYYY-mm-dd: ", end="", file=stderr)
+        date_str = input()
         if not is_valid_date(date_str):
             ft_write_error(f"{date_str} - Invalid date_str format")
     return date_str
