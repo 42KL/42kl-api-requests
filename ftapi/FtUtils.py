cb3e441adc04ad65340d61564bd2c95ae377cf85
yt <yealingt@42iskandarputeri.edu.my>
2025-08-20 11:37:25 +0800

    (chore) import only stderr from sys, remove usage of colorama

--- ftapi/FtUtils.py
+++ ftapi/FtUtils.py
@@ -1,26 +1,22 @@
 # utils.py
 """Commonly used handy little utility functions."""
 
-import colorama
-import sys
+from sys import stderr
 
 
 def ft_write_error(message: str):
     """Writes messages to the screen (stderr) using red font."""
-    colorama.init(autoreset=True)
-    print(f"{colorama.Fore.RED}{message}", file=sys.stderr)
+    print(f"\033[31m{message}\033[0m", file=stderr)
     return
 
 
 def ft_write_info(message: str):
     """Writes messages to the screen (stderr) using yellow font."""
-    colorama.init(autoreset=True)
-    print(f"{colorama.Fore.YELLOW}[INFO] {message}", file=sys.stderr)
+    print(f"\033[33m[INFO] {message}\033[0m", file=stderr)
     return
 
 
 def ft_write_success(message: str):
     """Writes messages to the screen (stderr) using green font."""
-    colorama.init(autoreset=True)
-    print(f"{colorama.Fore.GREEN}[SUCCESS] {message}", file=sys.stderr)
+    print(f"\033[32m[SUCCESS] {message}\033[0m", file=stderr)
     return
